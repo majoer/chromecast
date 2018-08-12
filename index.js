37c07ad84b7854a02d5af2aa0b1291a4a3b84140
Mats Jørgensen <matsjoer@gmail.com>
2018-08-12 23:51:11 +0200

    Add ErrorCode to index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-module.exports = require('./src/Chromecast');
+const ErrorCode = require('./src/ErrorCode');
+const Chromecast = require('./src/Chromecast');
+
+Chromecast.ErrorCode = ErrorCode;
+
+module.exports = Chromecast;
