ebaace5d909e529c6025484419073b25fb3ce56b
Mats Jørgensen <matsjoer@gmail.com>
2018-08-12 23:55:37 +0200

    Add domain objects to index export

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const ErrorCode = require('./src/ErrorCode');
 const Chromecast = require('./src/Chromecast');
 
-Chromecast.ErrorCode = ErrorCode;
+Chromecast.ErrorCode = require('./src/ErrorCode');
+Chromecast.ChromecastMedia = require('./src/ChromecastMedia');
+Chromecast.ChromecastQueueMedia = require('./src/ChromecastQueueMedia');
 
 module.exports = Chromecast;
